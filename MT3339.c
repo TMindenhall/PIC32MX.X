2f4429273f701da194ffb5be90344020097a9afc
TMindenhall <tminden1@gmail.com>
2019-03-22 14:10:56 -0600

    Added Parsing for GPS

--- MT3339.c
+++ MT3339.c
@@ -33,24 +33,257 @@ void GPS_String_Handler(void) {
 
 }
 
-void GPS_Parse_NMEA(char *NMEA_ptr) {
-    //    char *comma_ptr = ",";
-    //    char *ask_ptr = "*";
-    //    char *token;
-    //    token = strtok(NMEA_ptr,comma_ptr);
-    ////    Send_String_U1(token);
-    //    while(token != NULL){
-    //       
-    //       Send_String_U1(token);
-    //       token = strtok(NULL,comma_ptr);
-    //    }
-    char *ptr = NMEA_Xfer_Buff;
-    Xfer_2D(ptr); //i = 0;
+char GPS_Parse_NMEA(char *nmea_ptr) {
+  // first look if we even have one
+  if (nmea_ptr[strlen(nmea_ptr)-4] == '*') {
+    uint16_t sum = parseHex(nmea_ptr[strlen(nmea_ptr)-3]) * 16;
+    sum += parseHex(nmea_ptr[strlen(nmea_ptr)-2]);
+    uint8_t i;
+    // check checksum
+    for (i=2; i < (strlen(nmea_ptr)-4); i++) {
+      sum ^= nmea_ptr[i];
+    }
+    if (sum != 0) {
+      // bad checksum :(
+      return -1;
+    }
+  }
+  
+  int32_t degree;
+  long minutes;
+  char degreebuff[10];
+  /////////////////////////////////////////////////////////////////////////////
+  //*******************************GPGGA************************************///
+  /////////////////////////////////////////////////////////////////////////////
+  
+  // look for a few common sentences
+  if (strstr(nmea_ptr, "$GPGGA")) {
+    // found GGA
+    char *p = nmea_ptr;
+    // get time
+    p = strchr(p, ',')+1;
+    float timef = atof(p);
+    uint32_t time = timef;
+    hour = time / 10000;
+    minute = (time % 10000) / 100;
+    seconds = (time % 100);
 
-    Send_String_U1(NMEA_Buffer_2[0]);
-    Send_String_U1(NMEA_Buffer_2[1]);
+    milliseconds = fmod(timef, 1.0) * 1000;
+
+    // parse out latitude
+    p = strchr(p, ',')+1;
+    if (',' != *p)
+    {
+      strncpy(degreebuff, p, 2);
+      p += 2;
+      degreebuff[2] = '\0';
+      degree = atol(degreebuff) * 10000000;
+      strncpy(degreebuff, p, 2); // minutes
+      p += 3; // skip decimal point
+      strncpy(degreebuff + 2, p, 4);
+      degreebuff[6] = '\0';
+      minutes = 50 * atol(degreebuff) / 3;
+      latitude_fixed = degree + minutes;
+      latitude = degree / 100000 + minutes * 0.000006F;
+      latitudeDegrees = (latitude-100*(int)(latitude/100))/60.0;
+      latitudeDegrees += (int)(latitude/100);
+    }
+
+    p = strchr(p, ',')+1;
+    if (',' != *p)
+    {
+      if (p[0] == 'S') latitudeDegrees *= -1.0;
+      if (p[0] == 'N') lat = 'N';
+      else if (p[0] == 'S') lat = 'S';
+      else if (p[0] == ',') lat = 0;
+      else return -1;
+    }
+
+    // parse out longitude
+    p = strchr(p, ',')+1;
+    if (',' != *p)
+    {
+      strncpy(degreebuff, p, 3);
+      p += 3;
+      degreebuff[3] = '\0';
+      degree = atol(degreebuff) * 10000000;
+      strncpy(degreebuff, p, 2); // minutes
+      p += 3; // skip decimal point
+      strncpy(degreebuff + 2, p, 4);
+      degreebuff[6] = '\0';
+      minutes = 50 * atol(degreebuff) / 3;
+      longitude_fixed = degree + minutes;
+      longitude = degree / 100000 + minutes * 0.000006F;
+      longitudeDegrees = (longitude-100*(int)(longitude/100))/60.0;
+      longitudeDegrees += (int)(longitude/100);
+    }
+
+    p = strchr(p, ',')+1;
+    if (',' != *p)
+    {
+      if (p[0] == 'W') longitudeDegrees *= -1.0;
+      if (p[0] == 'W') lon = 'W';
+      else if (p[0] == 'E') lon = 'E';
+      else if (p[0] == ',') lon = 0;
+      else return -1;
+    }
+
+    p = strchr(p, ',')+1;
+    if (',' != *p)
+    {
+      fixquality = atoi(p);
+    }
+
+    p = strchr(p, ',')+1;
+    if (',' != *p)
+    {
+      satellites = atoi(p);
+    }
+
+    p = strchr(p, ',')+1;
+    if (',' != *p)
+    {
+      HDOP = atof(p);
+    }
+
+    p = strchr(p, ',')+1;
+    if (',' != *p)
+    {
+      altitude = atof(p);
+    }
+
+    p = strchr(p, ',')+1;
+    p = strchr(p, ',')+1;
+    if (',' != *p)
+    {
+      geoidheight = atof(p);
+    }
+    return 1;
+  }
+  //////////////////////////////////////////////////////////////////////////////
+  //*****************************GPRMC****************************************//
+  //////////////////////////////////////////////////////////////////////////////
+  if (strstr(nmea_ptr, "$GPRMC")) {
+   // found RMC
+    char *p = nmea_ptr;
+
+    // get time
+    p = strchr(p, ',')+1;
+    float timef = atof(p);
+    uint32_t time = timef;
+    hour = time / 10000;
+    minute = (time % 10000) / 100;
+    seconds = (time % 100);
+
+    milliseconds = fmod(timef, 1.0) * 1000;
+
+    p = strchr(p, ',')+1;
+    // Serial.println(p);
+    if (p[0] == 'A')
+      fix = 1;
+    else if (p[0] == 'V')
+      fix = -1;
+    else
+      return -1;
+
+    // parse out latitude
+    p = strchr(p, ',')+1;
+    if (',' != *p)
+    {
+      strncpy(degreebuff, p, 2);
+      p += 2;
+      degreebuff[2] = '\0';
+      long degree = atol(degreebuff) * 10000000;
+      strncpy(degreebuff, p, 2); // minutes
+      p += 3; // skip decimal point
+      strncpy(degreebuff + 2, p, 4);
+      degreebuff[6] = '\0';
+      long minutes = 50 * atol(degreebuff) / 3;
+      latitude_fixed = degree + minutes;
+      latitude = degree / 100000 + minutes * 0.000006F;
+      latitudeDegrees = (latitude-100*(int)(latitude/100))/60.0;
+      latitudeDegrees += (int)(latitude/100);
+    }
+
+    p = strchr(p, ',')+1;
+    if (',' != *p)
+    {
+      if (p[0] == 'S') latitudeDegrees *= -1.0;
+      if (p[0] == 'N') lat = 'N';
+      else if (p[0] == 'S') lat = 'S';
+      else if (p[0] == ',') lat = 0;
+      else return -1;
+    }
+
+    // parse out longitude
+    p = strchr(p, ',')+1;
+    if (',' != *p)
+    {
+      strncpy(degreebuff, p, 3);
+      p += 3;
+      degreebuff[3] = '\0';
+      degree = atol(degreebuff) * 10000000;
+      strncpy(degreebuff, p, 2); // minutes
+      p += 3; // skip decimal point
+      strncpy(degreebuff + 2, p, 4);
+      degreebuff[6] = '\0';
+      minutes = 50 * atol(degreebuff) / 3;
+      longitude_fixed = degree + minutes;
+      longitude = degree / 100000 + minutes * 0.000006F;
+      longitudeDegrees = (longitude-100*(int)(longitude/100))/60.0;
+      longitudeDegrees += (int)(longitude/100);
+    }
+
+    p = strchr(p, ',')+1;
+    if (',' != *p)
+    {
+      if (p[0] == 'W') longitudeDegrees *= -1.0;
+      if (p[0] == 'W') lon = 'W';
+      else if (p[0] == 'E') lon = 'E';
+      else if (p[0] == ',') lon = 0;
+      else return -1;
+    }
+    // speed
+    p = strchr(p, ',')+1;
+    if (',' != *p)
+    {
+      speed = atof(p);
+    }
+
+    // angle
+    p = strchr(p, ',')+1;
+    if (',' != *p)
+    {
+      angle = atof(p);
+    }
+
+    p = strchr(p, ',')+1;
+    if (',' != *p)
+    {
+      uint32_t fulldate = atof(p);
+      day = fulldate / 10000;
+      month = (fulldate % 10000) / 100;
+      year = (fulldate % 100);
+    }
+    // we dont parse the remaining, yet!
+    return 1;
+  }
+
+  return -1;
+
 }
-
+uint8_t parseHex(char c) {
+    if (c < '0')
+      return 0;
+    if (c <= '9')
+      return c - '0';
+    if (c < 'A')
+       return 0;
+    if (c <= 'F')
+       return (c - 'A')+10;
+    // if (c > 'F')
+    return 0;
+}
 void Xfer_String(char *from_ptr, char *to_ptr) {
 
     char length = sizeof (NMEA_Xfer_Buff);
